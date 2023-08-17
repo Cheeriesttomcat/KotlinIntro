9c752ca8477363f2cd0564e24ba83ce81604e5b5
Howard Akers <64698227+Cheeriesttomcat@users.noreply.github.com>
2023-08-17 14:16:44 -0400

    Update happy.kt

--- happy.kt
+++ happy.kt
@@ -4,6 +4,11 @@ fun main() {
     printBorder(border,timesToRepeat)
     println("  Happy Birthday, Jhansi!")
     printBorder(border,timesToRepeat)
+    val age = 24
+    val layers = 5
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
 }
 
 fun printBorder(border: String, times: Int) {
@@ -11,4 +16,33 @@ fun printBorder(border: String, times: Int) {
         print(border)
     }
     println()
+}
+
+fun printCakeTop(age: Int) {
+    repeat(age + 2) {
+        print("=")
+    }
+    println()
+}
+
+fun printCakeCandles(age: Int) {
+    print(" ")
+    repeat(age) {
+        print(",")
+    }
+    println() // Print an empty line
+
+    print(" ") // Print the inset of the candles on the cake
+    repeat(age) {
+        print("|")
+    }
+    println()
+}
+fun printCakeBottom(age: Int, layers: Int) {
+    repeat(layers){
+        repeat(age + 2) {
+            print("@")
+        }
+        println()
+    }
 }
