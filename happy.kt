d333587e45516e2d18167500e6e5d4f125925fe3
Howard Akers <64698227+Cheeriesttomcat@users.noreply.github.com>
2023-08-17 13:53:07 -0400

    Update happy.kt

--- happy.kt
+++ happy.kt
@@ -2,11 +2,15 @@ fun main() {
     println("Happy Birthday, Rover!")
     println("You are already 5!")
     println("   ,,,,,   ")
+
+    // Let's print a cake!
     println("   |||||   ")
     println(" =========")
     println("@@@@@@@@@@@")
     println("{~@~@~@~@~}")
     println("@@@@@@@@@@@")
+
+    //This is empty
     println("")
     println("5 is the very best age to celebrate!")
 }
